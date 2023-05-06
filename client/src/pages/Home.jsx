03aa272fbb180f00a55526537f3c7fca5d2eb5eb
Tina y <17134939+tyousofine@users.noreply.github.com>
2023-05-05 20:42:03 -0400

    reversing changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,8 +29,8 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://salvador-three.vercel.app/api/v1/post', {
-      // const response = await fetch('https://salvador.onrender.com/api/v1/post', {
+
+      const response = await fetch('https://salvador.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
